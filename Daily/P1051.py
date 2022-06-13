e0779411e77aac8836c07cbe3c4aaa1d85b03cd7
吕昌泽 <1971607150@qq.com>
2022-06-13 15:00:57 +0800

    1

--- Daily/P1051.py
+++ Daily/P1051.py
@@ -3,3 +3,16 @@ from typing import List
 
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
+        heights_sorted = heights.copy()
+        list.sort(heights)
+        print(heights_sorted)
+        n = 0
+        for i in range(len(heights)):
+            if heights[i] != heights_sorted[i]:
+                n += 1
+        return n
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.heightChecker([1, 1, 4, 2, 1, 3]))
